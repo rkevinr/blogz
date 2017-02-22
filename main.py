aa889e4f1e314bfb57a2db76d79b063ed7e8455c
rkevinr <rkevinrnb@gmail.com>
2017-02-22 09:30:45 -0600

    login fixes and misc

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ class BlogHandler(webapp2.RequestHandler):
             The user parameter will be a User object.
         """
 
-        # TODO - filter the query so that only posts by the given user
-        # indiv_user_posts = self.get_posts(limit, offset).filter("author", self.user)
         indiv_user_posts = Post.all().filter("author", user).order('-created')
         return indiv_user_posts.fetch(limit=limit, offset=offset)
 
@@ -262,11 +260,10 @@ class LoginHandler(BlogHandler):
 
     # TODO - The login code here is mostly set up for you, but there isn't a template to log in
 
-    def render_login_form(self, error=""):
+    def render_login_form(self, errors={}, username=""):
         """ Render the login form with or without an error, based on parameters """
         t = jinja_env.get_template("login.html")
-        # response = t.render(error=error)
-        response = t.render(errors=[])
+        response = t.render(errors=errors, username=username)
         self.response.out.write(response)
 
     def get(self):
@@ -279,13 +276,24 @@ class LoginHandler(BlogHandler):
         # get the user from the database
         user = self.get_user_by_name(submitted_username)
 
+        '''
+        errors['username_error'] = "That's not a valid username"
+        errors['password_error'] = "That's not a valid password"
+        '''
+        errors = {}
+
         if not user:
-            self.render_login_form(error="Invalid username")
+            errors['username_error'] = "Invalid username"
+            self.render_login_form(errors=errors, username=submitted_username)
+
         elif hashutils.valid_pw(submitted_username, submitted_password, user.pw_hash):
             self.login_user(user)
             self.redirect('/blog/newpost')
+
         else:
-            self.render_login_form(error="Invalid password")
+            errors['password_error'] = "Invalid password"
+            self.render_login_form(errors=errors, username=submitted_username)
+
 
 class LogoutHandler(BlogHandler):
 
